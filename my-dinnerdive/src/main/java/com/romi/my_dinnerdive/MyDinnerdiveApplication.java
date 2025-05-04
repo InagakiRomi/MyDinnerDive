15bf68e45853f8ad3bef1bdd4ceea51fe6f47601
InagakiRomi <100176049+InagakiRomi@users.noreply.github.com>
2025-05-04 16:41:38 +0800

    [+]初始化專案

---
+++ my-dinnerdive/src/main/java/com/romi/my_dinnerdive/MyDinnerdiveApplication.java
@@ -0,0 +1,13 @@
+package com.romi.my_dinnerdive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDinnerdiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDinnerdiveApplication.class, args);
+	}
+
+}
---
+++ my-dinnerdive/src/test/java/com/romi/my_dinnerdive/MyDinnerdiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romi.my_dinnerdive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDinnerdiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
