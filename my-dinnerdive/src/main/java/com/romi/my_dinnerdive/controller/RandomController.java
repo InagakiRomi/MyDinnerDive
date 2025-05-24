b9a3455a6a0a45e805b002b6ec8bf2b66ac2bf21
InagakiRomi <100176049+InagakiRomi@users.noreply.github.com>
2025-05-24 23:33:01 +0800

    [+]抽籤時圖片動態效果

--- my-dinnerdive/src/main/java/com/romi/my_dinnerdive/controller/RandomController.java
+++ my-dinnerdive/src/main/java/com/romi/my_dinnerdive/controller/RandomController.java
@@ -2,8 +2,12 @@ package com.romi.my_dinnerdive.controller;
 
 import com.romi.my_dinnerdive.model.Restaurant;
 import com.romi.my_dinnerdive.service.RestaurantService;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
