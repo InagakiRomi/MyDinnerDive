10acec1ac5803f913b6d55df157594b643ccc7b0
InagakiRomi <100176049+InagakiRomi@users.noreply.github.com>
2025-06-09 18:39:03 +0800

    [!]修改介面新增回首頁按鈕

--- my-dinnerdive/src/main/java/com/romi/my_dinnerdive/ThymeleafController.java
+++ my-dinnerdive/src/main/java/com/romi/my_dinnerdive/ThymeleafController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import com.romi.my_dinnerdive.model.Restaurant;
 import com.romi.my_dinnerdive.service.RestaurantService;
