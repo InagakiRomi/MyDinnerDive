abc1cc63adf666591ff334363c6ecb33b76c0c8e
InagakiRomi <100176049+InagakiRomi@users.noreply.github.com>
2025-06-11 00:04:24 +0800

    [!]調整html檔案

--- my-dinnerdive/src/main/resources/static/js/randomRestaurant.js
+++ my-dinnerdive/src/main/resources/static/js/randomRestaurant.js
@@ -1,13 +1,29 @@
+let categoryDisplayNameMap = {};
+
+fetch('/categories')
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(category => {
+            categoryDisplayNameMap[category.name] = category.displayName;
+        });
+
+        getRandomRestaurant();
+    })
+    .catch(err => console.error('載入分類失敗：', err));
+
 function getRandomRestaurant() {
     fetch('/random')
         .then(res => res.json())
         .then(data => {
             document.getElementById('updateImg').src = data.imageUrl;
             document.getElementById('updateName').innerText = data.restaurantName;
-            document.getElementById('updateCategory').innerText = data.category;
+
+            const displayCategory = categoryDisplayNameMap[data.category] || data.category;
+            document.getElementById('updateCategory').innerText = displayCategory;
+
             document.getElementById('updateVisitedCount').innerText = data.visitedCount;
             document.getElementById('updateLastEat').innerText = data.lastEat;
             document.getElementById('updateLastVisitedAt').innerText = data.lastVisitedAt;
             document.getElementById('updateNote').innerText = data.note;
-        });
+        })
 }
