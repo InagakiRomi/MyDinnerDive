25fbed369489327b12208fd294485158962863dc
InagakiRomi <100176049+InagakiRomi@users.noreply.github.com>
2025-06-17 18:28:35 +0800

    [!]調整list js程式碼

--- my-dinnerdive/src/main/resources/static/js/listRestaurant.js
+++ my-dinnerdive/src/main/resources/static/js/listRestaurant.js
@@ -1,4 +1,54 @@
-document.getElementById("filter-form").addEventListener("submit", function (e) {
+document.addEventListener("DOMContentLoaded", listRestaurant);
+
+function listRestaurant(){
+    const data = [
+        {
+            restaurantName: "兩藍拉麵",
+            imageUrl: "https://images.unsplash.com/photo-1614563637806-1d0e645e0940?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            category: "主食",
+            visitedCount: 2,
+            lastEat: "2025-06-06 17:37:50",
+            lastVisitedAt: "2025-06-06 17:37:50",
+            note: "兩藍拉麵好吃啦"
+        },
+        {
+            restaurantName: "兩藍拉麵",
+            imageUrl: "https://images.unsplash.com/photo-1614563637806-1d0e645e0940?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            category: "主食",
+            visitedCount: 2,
+            lastEat: "2025-06-06 17:37:50",
+            lastVisitedAt: "2025-06-06 17:37:50",
+            note: "兩藍拉麵好吃啦"
+        }
+    ];
+
+    const tbody = document.getElementById('tableBody');
+
+    data.forEach(restaurants => {
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+            <td>${restaurants.restaurantName}</td>
+            <td>
+                <img class="listImage"
+                 src="${restaurants.imageUrl}"
+                 alt="餐廳圖片"
+                 width="100"/>
+            </td>
+            <td>${restaurants.category}</td>
+            <td>${restaurants.visitedCount}</td>
+            <td>${restaurants.lastEat}</td>
+            <td>${restaurants.lastVisitedAt}</td>
+            <td>${restaurants.note}</td>
+            <td>
+                <button>修改</button>
+                <button style="margin-left: 5px;">刪除</button>
+            </td>
+        `;
+        tbody.appendChild(tr);
+    });
+}
+
+/*document.getElementById("filter-form").addEventListener("submit", function (e) {
     e.preventDefault(); // 阻止表單重新載入頁面
 
     const category = document.getElementById("category").value;
@@ -72,4 +122,4 @@ function loadRestaurants(category, search) {
                 });
             });
         });
-}
+}*/
