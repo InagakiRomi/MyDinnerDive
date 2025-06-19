e9a866b83b6c02f3be2797d37cc51aa1360b9c3c
InagakiRomi <100176049+InagakiRomi@users.noreply.github.com>
2025-06-19 18:35:03 +0800

    [!]選擇餐廳前端

---
+++ my-dinnerdive/src/main/resources/static/js/chooseRestaurant.js
@@ -0,0 +1,35 @@
+import {
+    getRestaurantId,
+    getHeaders
+} from './modules/restaurantDataBuilder.js';
+
+document.addEventListener("DOMContentLoaded", function (){
+    const preventForm = document.getElementById("chooseBtn");
+    preventForm.addEventListener("submit", preventFormSubmit);
+
+    function preventFormSubmit(event) {
+        event.preventDefault();
+        chooseRestaurant();
+    }
+})
+
+function chooseRestaurant(){
+
+    fetch(`/choose/${getRestaurantId()}`, {
+        method: 'PUT',
+        headers: getHeaders(),
+        body: JSON.stringify(restaurantJson)
+    })
+    .then(response => {
+        if (response.ok) {
+            window.location.href = "/dinnerHome";
+            alert("選擇成功！");
+        } else {
+            alert("選擇失敗！請確認資料是否正確");
+        }
+    })
+    .catch(error => {
+        console.error("Error:", error);
+        alert("發生錯誤，請稍後再試！");
+    });
+}
