64dd931484846d36d9c76eff9813a8e1cd8bead5
InagakiRomi <100176049+InagakiRomi@users.noreply.github.com>
2025-07-07 10:13:01 +0800

    [!]調整單元測試

--- my-dinnerdive/src/test/java/com/romi/my_dinnerdive/MyDinnerdiveApplicationTests.java
+++ my-dinnerdive/src/test/java/com/romi/my_dinnerdive/MyDinnerdiveApplicationTests.java
@@ -1,6 +1,6 @@
 package com.romi.my_dinnerdive;
 
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
